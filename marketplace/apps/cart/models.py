ba046da2708872db78b62c3c54b20d2240a1564a
ycaro <ycaro00287@gmail.com>
2023-11-23 10:19:54 -0300

    todos

--- marketplace/apps/cart/models.py
+++ marketplace/apps/cart/models.py
@@ -21,7 +21,7 @@ class CartItem(models.Model):
 
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=256)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
 
